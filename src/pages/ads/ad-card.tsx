188f8f5bfc5a15dbac57b6ed3c5c20faaf978c76
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-05-27 19:30:21 +0200

    feat: AdCard component finished

--- src/pages/ads/ad-card.tsx
+++ src/pages/ads/ad-card.tsx
@@ -15,6 +15,21 @@ const AdCard = ({ advert }: AdCardProps) => {
       </div>
 
       <SellBuyTag sale={sale} />
+
+      <div className="ad-info">
+        <p className="ad-price">{price} EUR</p>
+        <p className="ad-name">
+          {name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
+        </p>
+      </div>
+
+      <div className="ad-tags">
+        {tags.map((tag) => (
+          <span key={tag} className="tag">
+            {tag}
+          </span>
+        ))}
+      </div>
     </article>
   );
 };
